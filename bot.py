6a0f023acacf3ecb8e03a845c8a6af51ecc15444
TANAT96564 <85154556+TANAT96564@users.noreply.github.com>
2021-06-06 19:40:28 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -30,7 +30,7 @@ async def on_ready():
 now = datetime.datetime.now()
 time = f"{str(now.year)}년 {str(now.month)}월 {str(now.day)}일 {str(now.hour)}시 {str(now.minute)}분"
 
-
+@client.event
 async def on_message_delete(message):
     if message.author.bot:return
     channel = client.get_channel(849536197273059338)
