c3ce997700c7b74979635f104942c83f08c10432
TANAT96564 <85154556+TANAT96564@users.noreply.github.com>
2021-06-08 11:52:06 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,24 +39,7 @@ async def on_message_delete(message):#메세지가 삭제 되면
     embed.set_footer(text=f"TNS 봇 | {time}")
     await channel.send(embed=embed)
 
-@client.event
-async def on_message(message) :
-    if message.content.startswith('주사위'):
 
-        randomNum = random.randrange(1, 7) # 1~6까지 랜덤수
-        print(randomNum)
-        if randomNum == 1:
-            await client.send_message(message.channel, embed=discord.Embed(':one:')
-        if randomNum == 2:
-            await client.send_message(message.channel, embed=discord.Embed(':two:')
-        if randomNum ==3:
-            await client.send_message(message.channel, embed=discord.Embed(':three:')
-        if randomNum ==4:
-            await client.send_message(message.channel, embed=discord.Embed(':four:')
-        if randomNum ==5:
-            await client.send_message(message.channel, embed=discord.Embed(':five:')
-        if randomNum ==6:
-            await client.send_message(message.channel, embed=discord.Embed(':six:')
 @client.event    
 async def on_message_edit(before, after):#메세지 수정 되면(작동 안함)
     if message.author.bot:return
