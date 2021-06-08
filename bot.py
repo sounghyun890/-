694f811e137f35d21ee589a0aea258e4d0e6b088
TANAT96564 <85154556+TANAT96564@users.noreply.github.com>
2021-06-08 17:23:35 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -151,7 +151,7 @@ async def on_message(message):
             
     
     
-    if bad >= -65 :
+    if bad >= -64 :
         a = await message.channel.send(message.author.mention+"님의 메세지가 삭제 되었습니다.\n[사유:부적절한 언어 포함]")
         await message.delete() 
         await asyncio.sleep(7)
